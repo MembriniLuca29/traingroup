9ab51acfa4e74abda8a6b541c9cea8a80b984dcf
TaccLuc <tacc.luca@gmail.com>
2023-09-13 16:12:13 +0200

    Add: created stations table and populated it

---
+++ database/seeders/StationsTableSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Station;
+
+class StationsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        for ($i=0; $i < 10; $i++) { 
+            
+            $station = new Station();
+            $station->location = fake()->city();
+            $station->ticket_resale = rand(0, 1);
+            $station->snack_bar = rand(0, 1);
+            $station->save();
+        }
+    }
+}
