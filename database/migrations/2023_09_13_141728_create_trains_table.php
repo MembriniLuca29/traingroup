94f4eb5b001f3a031f547899233d03bc6a075880
Samuelec14 <samuconf14@gmail.com>
2023-09-13 16:21:37 +0200

    Train table migration commit

---
+++ database/migrations/2023_09_13_141728_create_trains_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('trains', function (Blueprint $table) {
+
+            /*
+            azienda                 stringa NOT NULL
+            stazione di partenza    stringa NOT NULL
+            stazione di arrivo      stringa NOT NULL
+            orario di partenza      time NOT NULL
+            orario di arrivo        time NOT NULL
+            codice treno            smallint NOT NULL
+            numero carrozze         small int NULL
+            in orario               tiny int -> booleano    DEFAULT TRUE
+            cancellato              tiny int -> booleano    DEFAULT FALSE
+            */
+
+            $table->id();
+            $table->string('azienda', 64);
+            $table->string('stazione_partenza', 64);
+            $table->string('stazione_arrivo', 64);
+            $table->string('codice_treno', 25);
+            $table->boolean('in_orario')->default(true);
+            $table->boolean('cancellato')->default(false);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('trains');
+    }
+};
